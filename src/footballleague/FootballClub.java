61ab658bea10e11679a9695b9ab10a72b2456955
mbartoszuk <mariabartoszuk@gmail.com>
2015-11-14 15:15:51 +0000

    Initial menu implementation
    Add, remove and display a football club.

--- src/footballleague/FootballClub.java
+++ src/footballleague/FootballClub.java
@@ -4,7 +4,30 @@ package footballleague;
 
 public class FootballClub {
     
+    String clubName;
+    String clubLocation;
+    
+    int numberOfWins;
+    int numberOfDraws;
+    int numberOfDefeats;
+    
+    int goalsReceived;
+    int goalsScored;
+    
+    int pointsCurrent;
+    int matchesPlayed;
+    
     public FootballClub(String clubName, String clubLocation) {
-        
+        this.clubName = clubName;
+        this.clubLocation = clubLocation;
+    }
+    
+    @Override
+    public String toString() {
+        return clubName + " " + clubLocation;
+    }
+    
+    public String getName() {
+        return clubName;
     }
 }
--- src/footballleague/PremierLeagueManager.java
+++ src/footballleague/PremierLeagueManager.java
@@ -9,15 +9,34 @@ import java.util.Scanner;
 public class PremierLeagueManager {
     
     List<FootballClub> clubsInPremierLeague = new ArrayList<>();
+    Scanner input = new Scanner(System.in);
     
     public void menu() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Please choose from the following options: ");
-        String choice = input.next();
+        System.out.println("Please choose from the following options: "
+            + "\nEnter A to Add a new club"
+            + "\nEnter D to Display statistics of a specific club"
+            + "\nEnter R to Remove an existing club"
+            + "\nEnter Q to Quit the program");
+        
+        boolean quit = false;
+        
+        while(!quit) {
+            String choice = input.next();
+            switch (choice) {
+                case "A": this.addFootballClub();
+                          break;
+                case "D": this.displayClubStats();
+                          break;
+                case "R": this.removeFootballClub();
+                          break;
+                case "Q": quit = true;
+                          break;
+                default: System.out.println("This option is invalid. Choose again.");
+            }
+        }
     }
     
     private void addFootballClub() {
-        Scanner input = new Scanner(System.in);
         System.out.println("Enter the name of a new club: ");
         String clubName = input.next();
         System.out.println("Enter the location of the new club: ");
@@ -26,7 +45,41 @@ public class PremierLeagueManager {
         clubsInPremierLeague.add(newClub);
     }
     
+    public FootballClub findClubByName(String clubName) {
+        for (FootballClub candidate : clubsInPremierLeague) {
+            String candidateName = candidate.getName();
+            if (clubName.equals(candidateName)) {
+                return candidate;
+            }
+        }
+        return null;
+    }
+    
+    private void displayClubStats() {
+        System.out.println("Enter the name of the club to see its statistics: ");
+        String clubName = input.next();
+        FootballClub club = findClubByName(clubName);
+        if (club == null) {
+            System.out.println("Requested club does not exist in this league.");
+        } else {
+            System.out.println(club);
+        }
+    }
+    
+    public void removeFootballClub() {
+        System.out.println("Enter the name of the club to remove it from the league: ");
+        String clubName = input.next();
+        FootballClub club = findClubByName(clubName);
+        if (club == null) {
+            System.out.println("Requested club does not exist in this league.");
+        } else {
+            this.clubsInPremierLeague.remove(club);
+            System.out.println(club + " was deleted from the league.");
+        }
+    }
+    
     public static void main(String [] args) {
-        
+        PremierLeagueManager test = new PremierLeagueManager();
+        test.menu();
     }
 }
